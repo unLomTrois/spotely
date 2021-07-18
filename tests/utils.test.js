ee1b18dc4682f6a06836290f4716524f15ae71a7
unlomtrois <vadim.kuz02@gmail.com>
2021-07-18 05:51:57 +0300

    add one more test for spotify validation

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -14,7 +14,9 @@ describe("link validation", () => {
 
     test("youtube link must have video id after v param", () => {
       expect(isYoutubeURL("https://youtube.com/watch?v=")).toBe(false);
-      expect(isYoutubeURL("https://youtube.com/watch?v=xUNqsfFUwhY")).toBe(true);
+      expect(isYoutubeURL("https://youtube.com/watch?v=xUNqsfFUwhY")).toBe(
+        true
+      );
     });
 
     test("basic youtu.be link", () => {
@@ -39,13 +41,22 @@ describe("link validation", () => {
 
     test("youtube music must have video id after v param", () => {
       expect(isYoutubeURL("https://music.youtube.com/watch?v=")).toBe(false);
-      expect(isYoutubeURL("https://music.youtube.com/watch?v=xUNqsfFUwhY")).toBe(true);
+      expect(
+        isYoutubeURL("https://music.youtube.com/watch?v=xUNqsfFUwhY")
+      ).toBe(true);
     });
   });
 
-  test("spotify url validation", () => {
-    expect(
-      isSpotifyURL("https://open.spotify.com/track/6dGnYIeXmHdcikdzNNDMm2")
-    ).toBe(true);
+  describe("spotify validation", () => {
+    test("spotify url validation", () => {
+      expect(
+        isSpotifyURL("https://open.spotify.com/track/6dGnYIeXmHdcikdzNNDMm2")
+      ).toBe(true);
+    });
+    
+    test("spotify link must have track with track id", () => {
+      expect(isSpotifyURL("https://open.spotify.com/")).toBe(false);
+      expect(isSpotifyURL("https://open.spotify.com/track/")).toBe(false);
+    });
   });
 });
