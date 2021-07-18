bbbd02a62c9a44cea96b59edb68af0f71704fea4
unlomtrois <vadim.kuz02@gmail.com>
2021-07-18 06:12:54 +0300

    fix link validation regexes to prevent substring attacks

--- src/utils.js
+++ src/utils.js
@@ -2,14 +2,14 @@ import fetch from "node-fetch";
 
 export const isSpotifyURL = (url) => {
   const spotify_link_regex = new RegExp(
-    /https?:\/\/open.spotify.com\/(track)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+|)/
+    /(?:http?s?:\/\/)?(?:www.)?open.spotify.com\/(track)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+|)/y
   );
   return spotify_link_regex.test(url);
 };
 
 export const isYoutubeURL = (url) => {
   const youtube_link_regex = new RegExp(
-    /(?:http?s?:\/\/)?(?:www.)?(?:m.)?(?:music.)?youtu(?:\.?be)(?:\.com)?(?:(?:\w*.?:\/\/)?\w*.?\w*-?.?\w*\/(?:embed|e|v|watch|.*\/)?\??(?:feature=\w*\.?\w*)?&?(?:v=)?\/?)([\w\d_-]{11})(?:\S+)?/gm
+    /(?:http?s?:\/\/)?(?:www.)?(?:m.)?(?:music.)?youtu(?:\.?be)(?:\.com)?(?:(?:\w*.?:\/\/)?\w*.?\w*-?.?\w*\/(?:embed|e|v|watch|.*\/)?\??(?:feature=\w*\.?\w*)?&?(?:v=)?\/?)([\w\d_-]{11})(?:\S+)?/y
   );
   return youtube_link_regex.test(url);
 };
